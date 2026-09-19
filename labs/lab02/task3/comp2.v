9202813ab4bb5be7a484c9c634f0572ebdc5b9a4
AdwaitDeshmukh7 <f20251318@pilani.bits-pilani.ac.in>
2026-09-19 09:38:11 +0000

    lab02 task3

--- labs/lab02/task3/comp2.v
+++ labs/lab02/task3/comp2.v
@@ -10,14 +10,14 @@
 
 module comp2 (
   input  [1:0] A,
-  input  [1:0] B, 
+  input  [1:0] B,
   output       GT,
   output       LT,
   output       EQ
 );
 
   assign EQ = (A == B);
-  assign GT = (A >= B);
-  assign LT = (A <  B); 
+  assign GT = (A > B);
+  assign LT = (A <  B);
 
 endmodule
